b1d491f3f662f4f2832fb2c73fa5fed7acc94cf9
Ralf Northman <ralf.northman@bredband.net>
2022-07-25 21:14:38 +0200

    M artists and N items.

--- randomizer/src/main.rs
+++ randomizer/src/main.rs
@@ -5,6 +5,9 @@ use std::iter::repeat_with;
 use std::{fs, io};
 
 fn main() -> io::Result<()> {
+    let n = fastrand::usize(..=4);
+    let m = fastrand::usize(..=4);
+
     let file_paths = fs::read_dir("lists")?
         .map(|res| res.map(|e| e.path()))
         .collect::<Result<Vec<_>, io::Error>>()?;
@@ -19,12 +22,24 @@ fn main() -> io::Result<()> {
     let v: Vec<&str> = contents.lines().collect();
 
     let rand_items: String = repeat_with(|| v[fastrand::usize(..v.len())])
-        .take(5)
+        .take(n)
         .map(|i| i.replace(" ", "-"))
         .intersperse(", ".to_string())
         .collect();
 
-    println!("{rand_items}");
+    let mut contents = String::new();
+    let mut file = File::open("people.txt")?;
+    file.read_to_string(&mut contents)?;
+
+    let v: Vec<&str> = contents.lines().collect();
+
+    let rand_people: String = repeat_with(|| v[fastrand::usize(..v.len())])
+        .take(m)
+        .map(|i| i.replace(" ", "-"))
+        .intersperse(", ".to_string())
+        .collect();
+
+    println!("\n--style {rand_people}, {rand_items}\n");
 
     Ok(())
 }
